bbfe7a3a69945427dcf1e1351bddfaa2946f057d
i5irin <contact@i5ir.in>
2023-11-12 23:38:39 +0900

    chore: add Prettier config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,14 +6,12 @@ module.exports = {
     sourceType: 'module',
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
-    'airbnb-base',
-    'airbnb-typescript/base',
-  ],
+  extends: ['airbnb-base', 'airbnb-typescript/base', 'prettier'],
   root: true,
   env: {
     browser: true,
     es2021: true,
     jest: true,
   },
+  ignorePatterns: ['!.prettierrc.cjs'],
 };
