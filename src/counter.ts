f9c767bc4f0b9705f9621ea04f51f70206564e83
i5irin <contact@i5ir.in>
2023-12-06 22:39:05 +0900

    chore: remove unnecessary files

--- src/counter.ts
+++
@@ -1,9 +0,0 @@
-export default function setupCounter(element: HTMLButtonElement) {
-  let counter = 0;
-  const setCounter = (count: number) => {
-    counter = count;
-    element.replaceChildren(document.createTextNode(`count is ${counter}`));
-  };
-  element.addEventListener('click', () => setCounter(counter + 1));
-  setCounter(0);
-}
